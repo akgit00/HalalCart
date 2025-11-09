ab0864f36a5b57dc2639a4f464391690a2779fff
akgit00 <tkfsecond@gmail.com>
2025-11-08 19:27:09 -0500

    Setting up the MenuItems class with constructors and getters

--- src/main/java/com/pluralsight/model/MenuItems.java
+++ src/main/java/com/pluralsight/model/MenuItems.java
@@ -1,4 +1,29 @@
 package com.pluralsight.model;
 
-public class MenuItems {
+public abstract class MenuItems {
+    protected String name;
+    protected String size;
+    protected double price;
+
+    //constructor
+    public MenuItems(String name, String size, double price) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract String getDescription();
 }
