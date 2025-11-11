69736b5c67d8b463e7a34cccf8986d0c82808fd1
akgit00 <tkfsecond@gmail.com>
2025-11-11 18:04:45 -0500

    finish saveReceipt method by using try and catch

--- src/main/java/com/pluralsight/model/Order.java
+++ src/main/java/com/pluralsight/model/Order.java
@@ -13,6 +13,7 @@ public class Order {
     public void addItem(MenuItems item) {
         items.add(item);
     }
+
     public double getTotal() {
         double total = 0;
         for (MenuItems item : items) total += item.getPrice();
@@ -25,5 +26,14 @@ public class Order {
 
     public void saveReceipt() {
         String filename = "receipts/" + new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date()) + ".txt";
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("Halal NYC Cart Order\n");
+            writer.write("=====================\n");
+            for (MenuItems item : items)
+                writer.write(item + "\n");
+            writer.write("\nTotal: $" + String.format("%.2f", getTotal()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
