ca7dff89b6d2fd8b798cd5c4b56da8a42ad7f2bd
akgit00 <tkfsecond@gmail.com>
2025-11-11 18:35:53 -0500

    add comments for next steps and create a label for a current order

--- src/main/java/com/pluralsight/ui/OrderPanel.java
+++ src/main/java/com/pluralsight/ui/OrderPanel.java
@@ -17,3 +17,23 @@ public class OrderPanel extends JPanel {
     public OrderPanel(MainFrame frame, Order order) {
         this.frame = frame;
         this.order = order;
+
+        setLayout(new BorderLayout(10, 10));
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        //title
+        JLabel title = new JLabel("Current Order", SwingConstants.CENTER);
+        title.setFont(new Font("SansSerif", Font.BOLD, 22));
+        add(title, BorderLayout.NORTH);
+
+        //item list go here
+
+        //buttons go here
+
+
+        //label to show the total goes here
+
+        //button action listeners go here
+
+
+        //--------helper methods go here--------
