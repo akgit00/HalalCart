555c0a44595b53a7b090089d9f3c200bbaf96f71
akgit00 <tkfsecond@gmail.com>
2025-11-11 14:03:23 -0500

    Create the OrderPanel class

---
+++ src/main/java/com/pluralsight/ui/OrderPanel.java
@@ -0,0 +1,11 @@
+package com.pluralsight.ui;
+
+import com.pluralsight.model.*;
+import com.pluralsight.model.MenuItems;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OrderPanel extends JPanel {
+
+}
