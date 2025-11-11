796fbd02376e161a60f2afe7e33257cfb26a45c6
akgit00 <tkfsecond@gmail.com>
2025-11-11 14:57:36 -0500

    start working on the home panel by determining the dimensions of it

--- src/main/java/com/pluralsight/ui/HomePanel.java
+++ src/main/java/com/pluralsight/ui/HomePanel.java
@@ -8,6 +8,12 @@ import java.awt.event.ActionListener;
 public class HomePanel extends JPanel {
 
     public HomePanel(MainFrame frame) {
+        //layout and dimensions of the home panel
+        setLayout(new BorderLayout());
+        JLabel title = new JLabel("Halal NYC Cart", SwingConstants.CENTER);
+        title.setFont(new Font("SansSerif", Font.BOLD, 28));
+        title.setBorder(BorderFactory.createEmptyBorder(40, 0, 20, 0));
+        add(title, BorderLayout.NORTH);
 
     }
 }
