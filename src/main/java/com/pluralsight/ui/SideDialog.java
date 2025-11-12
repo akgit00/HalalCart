4da277aa668216389b556984e2d59142b358e835
akgit00 <tkfsecond@gmail.com>
2025-11-12 15:07:35 -0500

    add a combo box for side options + define the labled private JPanel to properly label boxes

--- src/main/java/com/pluralsight/ui/SideDialog.java
+++ src/main/java/com/pluralsight/ui/SideDialog.java
@@ -10,12 +10,27 @@ public class SideDialog extends JDialog {
     private Side side;
     private boolean confirmed = false;
 
+    private final JComboBox<String> typeBox;
 
     public SideDialog(Frame owner) {
         super(owner, "Select a Side", true);
         setSize(300, 180);
-        setLayout(new BorderLayout(10,10));
+        setLayout(new BorderLayout(10, 10));
         setLocationRelativeTo(owner);
 
         JPanel form = new JPanel();
         form.setLayout(new BoxLayout(form, BoxLayout.Y_AXIS));
+
+        typeBox = new JComboBox<>(new String[]{"Fries", "Salad", "Pita Bread", "Falafel Balls"});
+        form.add(labeled("Side Item:", typeBox));
+
+        add(form, BorderLayout.CENTER);
+    }
+
+    private JPanel labeled(String text, JComponent c) {
+        JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        p.add(new JLabel(text));
+        p.add(c);
+        return p;
+    }
+}
