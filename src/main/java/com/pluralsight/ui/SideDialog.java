d53a4002c15a78933e49ddde9370bf242b9c3feb
akgit00 <tkfsecond@gmail.com>
2025-11-12 14:56:19 -0500

    create a new JPanel for selecting a side

--- src/main/java/com/pluralsight/ui/SideDialog.java
+++ src/main/java/com/pluralsight/ui/SideDialog.java
@@ -9,3 +9,13 @@ import java.awt.event.ActionEvent;
 public class SideDialog extends JDialog {
     private Side side;
     private boolean confirmed = false;
+
+
+    public SideDialog(Frame owner) {
+        super(owner, "Select a Side", true);
+        setSize(300, 180);
+        setLayout(new BorderLayout(10,10));
+        setLocationRelativeTo(owner);
+
+        JPanel form = new JPanel();
+        form.setLayout(new BoxLayout(form, BoxLayout.Y_AXIS));
