52a69dc37561bb2655526162cd50654c180bdee6
akgit00 <tkfsecond@gmail.com>
2025-11-12 18:08:31 -0500

    Create an email settings class for the user to store their email address

---
+++ src/main/java/com/pluralsight/ui/EmailSettingsDialog.java
@@ -0,0 +1,15 @@
+package com.pluralsight.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class EmailSettingsDialog extends JDialog {
+
+    private static final File SETTINGS_FILE = new File("private_logs/email_settings.txt");
+
+    private final JTextField emailField;
+    private final JPasswordField passwordField;
+
+    public EmailSettingsDialog(Frame owner) {
