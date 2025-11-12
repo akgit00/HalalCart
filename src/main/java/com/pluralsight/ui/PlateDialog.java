77616da47848ab7a383bdc7b30708ff94d4baed4
akgit00 <tkfsecond@gmail.com>
2025-11-11 20:09:13 -0500

    comments to help with next steps + plate customization setup

--- src/main/java/com/pluralsight/ui/PlateDialog.java
+++ src/main/java/com/pluralsight/ui/PlateDialog.java
@@ -23,3 +23,23 @@ public class PlateDialog extends JDialog {
     private final JCheckBox[] sauceChecks;
 
     public PlateDialog(Frame owner) {
+        super(owner, "Customize Your Halal Plate", true);
+        setLayout(new BorderLayout(10,10));
+        setSize(200, 400);
+        setLocationRelativeTo(owner);
+
+        //panel to choose plate options go here
+
+        //size goes here
+
+        //base type goes here
+
+        //protein options go here
+
+        //standard topping options go here
+
+        //special topping options go here
+
+        //sauces go here
+
+        //special options go here
