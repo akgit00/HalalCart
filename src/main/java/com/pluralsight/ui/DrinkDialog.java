bf7c44943d46c0a0b8641a834e65d4e7107edae1
akgit00 <tkfsecond@gmail.com>
2025-11-12 12:58:23 -0500

    start setting up the "select a drink" box

--- src/main/java/com/pluralsight/ui/DrinkDialog.java
+++ src/main/java/com/pluralsight/ui/DrinkDialog.java
@@ -9,4 +9,11 @@ import java.awt.event.ActionEvent;
 public class DrinkDialog extends JDialog {
     private Drink drink;
     private boolean confirmed = false;
+
+    public DrinkDialog(Frame owner) {
+        super(owner, "Select a Drink", true);
+        setSize(300, 200);
+        setLayout(new BorderLayout(10, 10));
+        setLocationRelativeTo(owner);
+    }
 }
