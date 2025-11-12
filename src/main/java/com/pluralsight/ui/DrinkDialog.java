79f2a17f39f58753cb3c8abec5bc7326be131bcc
akgit00 <tkfsecond@gmail.com>
2025-11-12 12:44:52 -0500

    create the DrinkDialog class to define drink options and buttons

---
+++ src/main/java/com/pluralsight/ui/DrinkDialog.java
@@ -0,0 +1,12 @@
+package com.pluralsight.ui;
+
+import com.pluralsight.model.Drink;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class DrinkDialog extends JDialog {
+    private Drink drink;
+    private boolean confirmed = false;
+}
