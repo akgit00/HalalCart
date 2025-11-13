f9d18f61bb2efe2c7dc179ff6f2966c37c77db32
akgit00 <tkfsecond@gmail.com>
2025-11-13 10:45:03 -0500

    create a new class that logs a user's private data such as email address and recepit

---
+++ src/main/java/com/pluralsight/util/PrivateDataLogger.java
@@ -0,0 +1,11 @@
+package com.pluralsight.util;
+
+import com.pluralsight.model.Order;
+import com.pluralsight.model.MenuItems;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PrivateDataLogger {
+
+}
