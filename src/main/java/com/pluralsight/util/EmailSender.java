85aa2494a5970752f750c67c1d6135319d539c20
akgit00 <tkfsecond@gmail.com>
2025-11-12 23:11:12 -0500

    add dependencies and plugins to pom.xml to support email sending + create a method for email configuration

--- src/main/java/com/pluralsight/util/EmailSender.java
+++ src/main/java/com/pluralsight/util/EmailSender.java
@@ -1,4 +1,16 @@
 package com.pluralsight.util;
 
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
 public class EmailSender {
-}
+    private final String senderEmail;
+    private final String senderPassword;
+
+    public EmailSender(String senderEmail, String senderPassword) {
+        this.senderEmail = senderEmail;
+        this.senderPassword = senderPassword;
+    }
