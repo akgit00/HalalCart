c72aeafbc179617946c93e70ec9a06ff9cf1aac4
akgit00 <tkfsecond@gmail.com>
2025-11-12 20:10:11 -0500

    create a title for user emaill configuration

--- src/main/java/com/pluralsight/ui/EmailSettingsDialog.java
+++ src/main/java/com/pluralsight/ui/EmailSettingsDialog.java
@@ -15,6 +15,16 @@ public class EmailSettingsDialog extends JDialog {
     public EmailSettingsDialog(Frame owner) {
         super(owner, "Email Settings", true);
 
+        // layout/dimensions
         setLayout(new BorderLayout(10, 10));
         setSize(400, 250);
         setLocationRelativeTo(owner);
+
+        //title for user email config
+        JLabel title = new JLabel("Configure Email Sender (Gmail only)", SwingConstants.CENTER);
+        title.setFont(new Font("SansSerif", Font.BOLD, 16));
+        add(title, BorderLayout.NORTH);
+
+        JPanel formPanel = new JPanel(new GridLayout(2, 2, 10, 10));
+        formPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
