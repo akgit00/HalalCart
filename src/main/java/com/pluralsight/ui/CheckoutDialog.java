961c4839afea4a32e695cead3283f07e09f1b2d0
akgit00 <tkfsecond@gmail.com>
2025-11-12 22:05:13 -0500

    create a title for checkout

--- src/main/java/com/pluralsight/ui/CheckoutDialog.java
+++ src/main/java/com/pluralsight/ui/CheckoutDialog.java
@@ -7,5 +7,15 @@ import java.awt.event.ActionEvent;
 import java.io.File;
 
 public class CheckoutDialog extends JDialog {
+    private final Order order;
 
+    public CheckoutDialog(Frame owner, Order order) {
+        super(owner, "Checkout", true);
+        this.order = order;
+
+        JLabel title = new JLabel("Checkout", SwingConstants.CENTER);
+        title.setFont(new Font("SansSerif", Font.BOLD, 22));
+        add(title, BorderLayout.NORTH);
+
+    }
 }
